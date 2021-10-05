af166ea28ed20c38d4891a77e2c69189167cd24f
Michael Fazio <mfazio23@gmail.com>
2021-10-05 11:22:01 -0500

    Add favicon and updates to the record book

---
+++ src/general/About.js
@@ -0,0 +1,6 @@
+export default function About() {
+    return (
+        <div>Icons made by <a href="https://www.flaticon.com/authors/justicon" title="justicon">justicon</a> from <a
+            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    )
+}
