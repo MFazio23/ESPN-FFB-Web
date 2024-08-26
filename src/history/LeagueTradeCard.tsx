2d23ea8c40e4296183eb2e82ab53d0a03250ec11
Michael Fazio <mfazio23@gmail.com>
2024-08-26 13:54:46 -0500

    Better trade cards

--- src/history/LeagueTradeCard.tsx
+++ src/history/LeagueTradeCard.tsx
@@ -1,10 +1,46 @@
-import {Card, CardContent} from '@mui/material';
+import {Card, CardContent, CardHeader, Divider, List, ListItem, Typography} from '@mui/material';
+import {Trade} from './LeagueTrades';
+import Box from '@mui/material/Box';
 
-export function LeagueTradeCard(props: { trade: string }) {
+interface LeagueTradeCardProps {
+    trade: Trade;
+}
+
+export function LeagueTradeCard({trade}: LeagueTradeCardProps) {
     //TODO: Make this look better
+    const {id, year, firstOwner, firstTeam, firstTeamPlayers, secondOwner, secondTeam, secondTeamPlayers} = trade;
     return <Card sx={{width: 400}}>
+        <CardHeader title={year}/>
         <CardContent>
-            {props.trade}
+            <Box display='flex' flexDirection='row' alignItems="center">
+                <Typography variant="h5">{firstOwner}</Typography>
+                <Typography variant="subtitle1" ml={1}>({firstTeam})</Typography>
+            </Box>
+            <Box display='flex' flexDirection={'row'} alignItems='center'>
+                <Typography variant='body1' sx={{opacity: '25%'}}>sent</Typography>
+                <List>
+                    {firstTeamPlayers.map(player => <ListItem
+                        key={player.name}>{player.name} ({player.team})</ListItem>)}
+                </List>
+            </Box>
+            <Divider/>
+            <Box display='flex' flexDirection='row' mt={1} alignItems="center">
+                <Typography variant="h5">{secondOwner}</Typography>
+                <Typography variant="subtitle1" ml={1}>({secondTeam})</Typography>
+            </Box>
+
+            <Box display='flex' flexDirection={'row'} alignItems='center'>
+                <Typography variant='body1' sx={{opacity: '25%'}}>sent</Typography>
+                <List>
+                    {secondTeamPlayers.map(player => <ListItem
+                        key={player.name}>{player.name} ({player.team})</ListItem>)}
+                </List>
+            </Box>
         </CardContent>
+        {/*<CardContent>
+            {year}: {firstOwner} ({firstTeam})
+            traded {firstTeamPlayers.map(player => `${player.name} (${player.team})`).join(", ")} to {secondOwner} ({secondTeam})
+            for {secondTeamPlayers.map(player => `${player.name} (${player.team})`).join(", ")}
+        </CardContent>*/}
     </Card>
 }
--- src/history/LeagueTrades.tsx
+++ src/history/LeagueTrades.tsx
@@ -2,7 +2,142 @@ import {Box} from '@mui/material';
 import HistoryPage, {HistoryPageCard} from './HistoryPage';
 import {LeagueTradeCard} from './LeagueTradeCard';
 
-const trades = [
+export interface Trade {
+    id: string;
+    year: number;
+    firstOwner: string;
+    firstTeam: string;
+    firstTeamPlayers: TradePlayer[];
+    secondOwner: string;
+    secondTeam: string;
+    secondTeamPlayers: TradePlayer[];
+}
+
+export interface TradePlayer {
+    name: string;
+    team: string;
+}
+
+const trades: Trade[] = [
+    {
+        id: "2023-Pelc-Fazio",
+        year: 2023,
+        firstOwner: "Ben Pelc",
+        firstTeam: "Joe Buck Yourself",
+        firstTeamPlayers: [{name: "Garrett Wilson", team: "NYJ"}],
+        secondOwner: "Michael Fazio",
+        secondTeam: "Am I Going Over?",
+        secondTeamPlayers: [{name: "Brian Robinson", team: "WAS"}, {name: "Christian Kirk", team: "JAX"}]
+    },
+    {
+        id: "2020-Otto-Fazio",
+        year: 2020,
+        firstOwner: "James Otto",
+        firstTeam: "Throw it I am Open",
+        firstTeamPlayers: [{name: "Jamaal Williams", team: "GB"}],
+        secondOwner: "Michael Fazio",
+        secondTeam: "Am I Going Over?",
+        secondTeamPlayers: [{name: "Justin Jackson", team: "LAC"}]
+    },
+    {
+        id: "2019-Ericsen-Johnson",
+        year: 2019,
+        firstOwner: "Ben Ericsen",
+        firstTeam: "Naptown Goiters",
+        firstTeamPlayers: [{name: "Tony Pollard", team: "DAL"}],
+        secondOwner: "Scott Johnson",
+        secondTeam: "Delicious Traitorade",
+        secondTeamPlayers: [{name: "Curtis Samuel", team: "CAR"}]
+    },
+    {
+        id: "2016-Pietrzykowski-Smith",
+        year: 2016,
+        firstOwner: "David Pietrzykowski",
+        firstTeam: "MarshallNomNom ForCutlersBalls",
+        firstTeamPlayers: [
+            {name: "LeGarrett Blount", team: "NE"},
+            {name: "Allen Robinson", team: "JAX"},
+            {name: "Mike Wallace", team: "BAL"}
+        ],
+        secondOwner: "Dave Smith",
+        secondTeam: "Waukesha Ochsenfroesche",
+        secondTeamPlayers: [{name: "Julio Jones", team: "ATL"}]
+    },
+    {
+        id: "2013-Fazio-Ericsen",
+        year: 2013,
+        firstOwner: "Michael Fazio",
+        firstTeam: "Not the Hammer",
+        firstTeamPlayers: [{name: "Randall Cobb", team: "GB"}],
+        secondOwner: "Tim Ericsen",
+        secondTeam: "Uncooked Box",
+        secondTeamPlayers: [{name: "LeSean McCoy", team: "PHI"}]
+    },
+    {
+        id: "2011-Breen-Otto",
+        year: 2011,
+        firstOwner: "David Breen",
+        firstTeam: "MKE BLOOOOOOBBBBB!!",
+        firstTeamPlayers: [
+            {name: "Pierre Thomas", team: "NO"},
+            {name: "Kevin Walter", team: "HOU"}
+        ],
+        secondOwner: "James Otto",
+        secondTeam: "Prepare to be Sacked",
+        secondTeamPlayers: [
+            {name: "Drew Brees", team: "NO"},
+            {name: "LaRod Stephens-Howling", team: "ARI"},
+        ]
+    },
+    {
+        id: "2010-Ericsen-Fazio",
+        year: 2010,
+        firstOwner: "Ben Ericsen",
+        firstTeam: "Naptown Goiters",
+        firstTeamPlayers: [{name: "Zach Miller", team: "OAK"}],
+        secondOwner: "Michael Fazio",
+        secondTeam: "Silver Fish Hand Catch",
+        secondTeamPlayers: [{name: "Visanthe Shiancoe", team: "MIN"}]
+    },
+    {
+        id: "2010-Otto-Gustafson",
+        year: 2010,
+        firstOwner: "James Otto",
+        firstTeam: "Wayne, NJ Dipstick Jimmys",
+        firstTeamPlayers: [{name: "Eli Manning", team: "NYG"}],
+        secondOwner: "Brandon Gustafson",
+        secondTeam: "Admiral Ackbar’s Team",
+        secondTeamPlayers: [{name: "John Carlson", team: "SEA"}]
+    },
+    {
+        id: "2009-Smith-Johnson",
+        year: 2009,
+        firstOwner: "Dave Smith",
+        firstTeam: "Waukesha Ochsenfroesche",
+        firstTeamPlayers: [{name: "Reggie Bush", team: "NO"}],
+        secondOwner: "Scott Johnson",
+        secondTeam: "The Milwaukee Singer",
+        secondTeamPlayers: [{name: "Devery Henderson", team: "NO"}]
+    },
+    {
+        id: "2009-Huang-Pietrzykowski",
+        year: 2009,
+        firstOwner: "Simon Huang",
+        firstTeam: "KungFu Panda",
+        firstTeamPlayers: [
+            {name: "Kevin Curtis", team: "PHI"},
+            {name: "Jerious Norwood", team: "ATL"}
+        ],
+        secondOwner: "David Pietrzykowski",
+        secondTeam: "treadhead for life",
+        secondTeamPlayers: [
+            {name: "Chris Chambers", team: "SD"},
+            {name: "Muhsin Muhammad", team: "CAR"}
+        ]
+    },
+]
+
+/*const trades = [
     "2023: Ben Pelc (Joe Buck Yourself) trade Garrett Wilson (NYJ) to Michael Fazio (Am I Going Over?) for Brian Robinson (WAS) and Christian Kirk (JAX)",
     "2020: James Otto (Throw it I am Open) trades Jamaal Williams (GB) to Michael Fazio (Am I Going Over?) for Justin Jackson (LAC)",
     "2019: Ben Ericsen (Naptown Goiters) trades Tony Pollard (DAL) to Scott Johnson (Delicious Traitorade) for Curtis Samuel (CAR)",
@@ -13,7 +148,7 @@ const trades = [
     "2010: James Otto (Wayne, NJ Dipstick Jimmys) trades Eli Manning (NYG) to Brandon Gustafson (Admiral Ackbar’s Team) for John Carlson (SEA)",
     "2009: Dave Smith (Waukesha Ochsenfroesche) trades Reggie Bush (NO) to Scott Johnson (The Milwaukee Singer) for Devery Henderson (NO)",
     "2009: Simon Huang (KungFu Panda) trades Kevin Curtis (PHI) and Jerious Norwood (ATL) to David Pietrzykowski (treadhead for life) for Chris Chambers (SD) and Muhsin Muhammad (CAR)",
-]
+]*/
 
 export default function LeagueTrades() {
     const header = (
@@ -25,7 +160,7 @@ export default function LeagueTrades() {
 
     const tradeCards = trades.map(trade => {
         const tradeCard: HistoryPageCard = {
-            id: trade,
+            id: trade.id,
             component: <LeagueTradeCard trade={trade}/>
         }
         return tradeCard;
