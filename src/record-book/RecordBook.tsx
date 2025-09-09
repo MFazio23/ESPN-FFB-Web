11eabc7260d59b53df06daad97a576eae12e05e1
Michael Fazio <mfazio23@gmail.com>
2025-09-09 07:57:48 -0500

    Remove unused import

--- src/record-book/RecordBook.tsx
+++ src/record-book/RecordBook.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import {useState} from 'react';
 import {Paper, Tab, Table, TableBody, TableCell, TableContainer, TableRow, Tabs, Typography} from "@mui/material";
 // @ts-ignore
